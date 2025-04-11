ac418dd99a7fc4b86f0408e687fa5bfdc3db3e22
kauevestena <kauemv2@gmail.com>
2025-04-11 18:24:15 -0300

    freezing

--- scripts/vmf3.py
+++ scripts/vmf3.py
@@ -33,6 +33,52 @@ logging.basicConfig(
 
 
 
+def calcular_derivadas_mapeamento(a, b, c, el):
+    """
+    Calcula a primeira e segunda derivadas da função de mapeamento VMF3
+    Retorna: (primeira_derivada, segunda_derivada)
+    """
+    sin_el = sin(el)
+    cos_el = cos(el)
+
+    # Termos intermediários
+    temp1 = sin_el + c
+    temp2 = sin_el + b / temp1
+    temp3 = sin_el + a / temp2
+
+    # Primeira derivada
+    term4 = (b * cos_el) / (temp1**2)
+    term5 = cos_el - term4
+    term6 = a * term5
+    term7 = temp2**2
+    term8 = term6 / term7
+    primeira_derivada = -(cos_el - term8) / (temp3**2)
+
+    # Segunda derivada (cálculo simplificado)
+    d_temp1 = cos_el
+    d_temp2 = cos_el - (b * d_temp1) / (temp1**2)
+    d_temp3 = cos_el - (a * d_temp2) / (temp2**2)
+
+    d_term4 = (-b * sin_el * (temp1 * 2) - 2 * b * cos_el * temp1 * d_temp1) / (
+        temp1 * 4
+    )
+    d_term5 = -sin_el - d_term4
+    d_term6 = a * d_term5
+    d_term7 = 2 * temp2 * d_temp2
+    d_term8 = (d_term6 * term7 - term6 * d_term7) / (
+        term7**2
+    )  # Corrigido: term7 em vez de temp7
+
+    d_numerador = -(-sin_el - d_term8)
+    d_denominador = 2 * temp3 * d_temp3
+
+    segunda_derivada = (
+        d_numerador * (temp3 * 2) - (-(cos_el - term8)) * d_denominador
+    ) / (temp3 * 4)
+
+    return primeira_derivada, segunda_derivada
+
+
 
 
 
@@ -56,7 +102,7 @@ def process(data_as_str, station, delaypath):
     lon_rad = lon * DEG2RAD
 
     # mfh, mfw, bh, bw, ch, cw, el = vmf3_ht(ah, aw, mjd, lat, lon, h_ell, zd)
-    mfh, mfw, _, _, _, _, el, _ = vmf3_ht(
+    mfh, mfw, bh, bw, ch, cw, el, doy = vmf3_ht(
         mjd=mjd, lat=lat_rad, lon=lon_rad, h_ell=h_ell, zd=zd, ah=ah, aw=aw
     )
 
@@ -73,12 +119,16 @@ def process(data_as_str, station, delaypath):
     sin_az = sin(az)
 
     mfw_grads = mfw * (gn_w * cos_az + ge_w * sin_az)
-    mfh_grads = mfh * (gn_h * cos_az + ge_h * sin_az)
+    # mfh_grads = mfh * (gn_h * cos_az + ge_h * sin_az)
 
 
     trop_corr_nograd = mfh * zhd + mfw * zwd
 
-    trop_corr = trop_corr_nograd + mfh_grads + mfw_grads
+    prim_deriv_w, _ = calcular_derivadas_mapeamento(
+        aw,bw,cw, el
+    )  
+
+    trop_corr = trop_corr_nograd + prim_deriv_w * (gn_w * cos_az + ge_w * sin_az)
 
 
     with open(delaypath, "a") as f:
